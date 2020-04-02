b8a6fb8fe5f684557f34a2ed157c2973e4f30190
Rahul Raj <rahulraj@Users-MacBook-Pro-2.local>
2020-04-02 10:11:28 +0530

    Added picocli, and log4j2 properties.

--- src/main/java/com/hashedin/redmask/RedMaskApp.java
+++ src/main/java/com/hashedin/redmask/RedMaskApp.java
@@ -1,6 +1,8 @@
 package com.hashedin.redmask;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Callable;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,24 +11,35 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hashedin.redmask.configurations.Configuration;
 import com.hashedin.redmask.service.MaskingService;
 
-public class RedMaskApp {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-  static Logger log = LogManager.getLogger(RedMaskApp.class);
+@Command(description = "Redmask Tool",
+  name = "redmask",
+  mixinStandardHelpOptions = true,
+  version = "redmask 1.0")
+public class RedMaskApp implements Callable<Integer>  {
 
-  public static void main( String[] args ) {
+  private static final Logger log = LogManager.getLogger(RedMaskApp.class);
+
+  @Option(names = {"-f", "--configfilepath"}, required = true,
+      description = "Complete file path of json containing masking configurations.")
+  private String configFilePath;
+
+  public static void main( String[] args ) throws IOException {
     log.info("Starting redmask application.");
+    System.exit(new CommandLine(new RedMaskApp()).execute(args));
+    log.info("Closing redmask application.");
+  }
 
-    String configFilePath = "";
+  @Override
+  public Integer call() throws Exception {
 
-    for (String val: args) {
-      configFilePath = val;
-    }
-    
-    ObjectMapper mapper = new ObjectMapper();
     Configuration config = null;
 
     try {
-      config = mapper.readValue(new File(configFilePath), Configuration.class);
+      config = new ObjectMapper().readValue(new File(configFilePath), Configuration.class);
     } catch (Exception ex) {
       log.error("Exception while reading config.json file: " + ex);
     }
@@ -34,7 +47,7 @@ public class RedMaskApp {
     MaskingService service = new MaskingService(config);
     service.createSchemaAndView();
     
-    log.info("Closing redmask application.");
+    return 0;
   }
 
 }
