b8039821a91b71bb47426e38f5d28c32a2f963bb
SanchayGupta1197 <sanchay.gupta@hashedin.com>
2020-04-06 12:08:29 +0530

    added DatePart function

--- src/main/resources/Date/DateOnly.sql
+++ src/main/resources/Date/DateOnly.sql
@@ -6,10 +6,30 @@ BEGIN
     CASE date_category
     when 'day' then
      EXECUTE format('select extract(day from timestamp %L)',ColName) into categoryval;
-    when 'month'
-    when 'year'
-    when '
-    RETURN make_timestamp(2000,1,categoryval::integer,0,0,0);
+    when 'month' then
+     EXECUTE format('select extract(month from timestamp %L)',ColName) into categoryval;
+    when 'year' then
+     EXECUTE format('select extract(year from timestamp %L)',ColName) into categoryval;
+    when 'hour'then
+     EXECUTE format('select extract(hour from timestamp %L)',ColName) into categoryval;
+    when 'minute' then
+     EXECUTE format('select extract(minute from timestamp %L)',ColName) into categoryval;
+    else
+     Return Colname;
+    End CAse;
+
+    CASE date_category
+    when 'day' then
+     RETURN make_timestamp(2000,1,categoryval::integer,1,0,0);
+    when 'month' then
+     RETURN make_timestamp(2000,categoryval::integer,1,1,0,0);
+    when 'year' then
+        RETURN make_timestamp(categoryval::integer,1,1,0,0,0);
+    when 'hour' then
+        RETURN make_timestamp(2000,1,1,categoryval::integer,0,0);
+    else
+        RETURN make_timestamp(2000,1,1,0,categoryval::integer,0);
+     End Case;
 
 END;
 $$
