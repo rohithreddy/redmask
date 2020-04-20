cca875518e16070c0c996e9f984cc9c111f7f15b
Rahul Raj <rahulraj@Users-MacBook-Pro-2.local>
2020-04-20 15:01:15 +0530

    Added RedMaskIT class and steps to write IT tests.

---
+++ src/test/java/com/hashedin/redmask/integration/RedMaskIT.java
@@ -0,0 +1,17 @@
+package com.hashedin.redmask.integration;
+
+public class RedMaskIT {
+
+  /**
+   * Steps for IT tests.
+   * 1. Start docker test Container.
+   * 2. Create needed DB in test DB.
+   * 3. Create table and load data into table.
+   * 4. Invoke redmask app with a masking config.
+   * 5. Verify if masked view is created.
+   * 
+   * Using developer user, not superuser.
+   * 6. Verify for masked data in respective column.
+   */
+
+}
